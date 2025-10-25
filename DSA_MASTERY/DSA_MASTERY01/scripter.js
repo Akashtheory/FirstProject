bad2d35b323b6a2182d328f3232cf7623217629c
Akashtheory <akashthawarewhy@gmail.com>
2025-10-25 22:27:39 +0530

    if else 6 questions

--- DSA_MASTERY/DSA_MASTERY01/scripter.js
+++ DSA_MASTERY/DSA_MASTERY01/scripter.js
@@ -1,11 +1,82 @@
-var a = 10;
-var b = 30;
+//1st question
+let age = 7;
 
-if(a<b){
-    console.log("A is bigger than B");
+if (age>=18){
+    console.log("You are an adult");
     
 }
 else{
-    console.log("B is bigger than A");
+    console.log("You are a minor");
+    
+}
+
+
+//2nd question
+
+let temp = 30;
+
+if(temp >=30){
+    console.log("It's Hot Outside");
+    
+}
+else{
+    console.log("It's not too hot");
+    
+}
+
+//3rd question
+
+let number = 99;
+
+if(number % 2 ===0){
+    console.log("The number is even");
+    
+}
+else{
+    console.log("The number is odd");
+    
+}
+
+//4th question
+
+let marks = 33;
+
+if(marks >= 33){
+    console.log("You passed the exam");
+    
+}
+else{
+    console.log("You failed the exam");
+    
+}
+
+
+// 5th question
+
+let ifrain = false;   // false means no raining, truth means raining  // 0 means false and 1 means truth
+
+if(ifrain){
+    console.log("Bring the umbrella");
+    
+}
+else{
+    console.log("No need for umbrella");
+    
+}
+
+// 6th question
+
+let time = 12;  // 2pm 
+
+if(time < 12){
+    console.log("Good Morning");
+    
+}
+else if(time < 18) {   // 6pm
+  console.log("Good Afternoon");
+  
+}
+else{
+    console.log("Good Night");
     
 }
