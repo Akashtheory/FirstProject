15916c7d6b3fa443b8e9a503a5a9c3ed9eea17d2
Akashtheory <akashthawarewhy@gmail.com>
2025-10-27 00:27:37 +0530

    for loop questions

--- DSA_MASTERY/DSA_MASTERY01/scripter.js
+++ DSA_MASTERY/DSA_MASTERY01/scripter.js
@@ -1,82 +1,195 @@
-//1st question
-let age = 7;
+// //1st question
+// let age = 7;
 
-if (age>=18){
-    console.log("You are an adult");
+// if (age>=18){
+//     console.log("You are an adult");
     
-}
-else{
-    console.log("You are a minor");
+// }
+// else{
+//     console.log("You are a minor");
     
-}
+// }
 
 
-//2nd question
+// //2nd question
 
-let temp = 30;
+// let temp = 30;
 
-if(temp >=30){
-    console.log("It's Hot Outside");
+// if(temp >=30){
+//     console.log("It's Hot Outside");
     
-}
-else{
-    console.log("It's not too hot");
+// }
+// else{
+//     console.log("It's not too hot");
     
-}
+// }
 
-//3rd question
+// //3rd question
 
-let number = 99;
+// let number = 99;
 
-if(number % 2 ===0){
-    console.log("The number is even");
+// if(number % 2 ===0){
+//     console.log("The number is even");
     
-}
-else{
-    console.log("The number is odd");
+// }
+// else{
+//     console.log("The number is odd");
     
-}
+// }
 
-//4th question
+// //4th question
 
-let marks = 33;
+// let marks = 33;
 
-if(marks >= 33){
-    console.log("You passed the exam");
+// if(marks >= 33){
+//     console.log("You passed the exam");
     
-}
-else{
-    console.log("You failed the exam");
+// }
+// else{
+//     console.log("You failed the exam");
     
-}
+// }
 
 
-// 5th question
+// // 5th question
 
-let ifrain = false;   // false means no raining, truth means raining  // 0 means false and 1 means truth
+// let ifrain = false;   // false means no raining, truth means raining  // 0 means false and 1 means truth
 
-if(ifrain){
-    console.log("Bring the umbrella");
+// if(ifrain){
+//     console.log("Bring the umbrella");
     
-}
-else{
-    console.log("No need for umbrella");
+// }
+// else{
+//     console.log("No need for umbrella");
     
-}
+// }
 
-// 6th question
+// // 6th question
 
-let time = 12;  // 2pm 
+// let time = 12;  // 2pm 
 
-if(time < 12){
-    console.log("Good Morning");
+// if(time < 12){
+//     console.log("Good Morning");
     
-}
-else if(time < 18) {   // 6pm
-  console.log("Good Afternoon");
+// }
+// else if(time < 18) {   // 6pm
+//   console.log("Good Afternoon");
   
-}
-else{
-    console.log("Good Night");
+// }
+// else{
+//     console.log("Good Night");
+    
+// }
+
+
+
+//Complex questions of for loop
+//1st complex question
+
+// for(let i =1; i<=5; i++){
+//     console.log(i);
+    
+// }
+
+// 2nd question
+// for(let m=2; m<=10; m+=2){
+//     console.log(m);
+    
+// }
+
+
+//3rd Question
+// for(let i = 10; i >=1; i--){
+//     console.log(i);
+    
+// }
+
+
+//4th Question
+// const alphabet = ['A', 'B', 'C', 'D', 'E', 'F'];
+
+// for(let i=0; i<=alphabet.length; i++){
+//     console.log(alphabet[i]);
+    
+// }
+
+//5th question
+
+// for(let i=1; i<=10; i+=2){
+//     console.log(i);
+    
+// }
+    
+
+//6th question
+// for(let i =1; i <=5; i++){
+//     console.log('2X' + i +'=' + (2*i));
+    
+// }
+
+
+//7th question
+// for(let i=1; i<=10; i++){
+//     console.log(i + 'squared is ' + (i *i));
+    
+// }
+
+
+//8th question
+
+// let sum = 0;
+// for(let i = 1; i <= 5; i++){
+//     sum += i;
+//     console.log('Sum is' + sum);
+// }
+
+
+//9th question
+
+// for(let i =1; i<=5; i++){
+//     let stars= '';
+//     for(let j =1; j<=i; j++){
+//         stars += '*';
+//     }
+//     console.log(stars);
+    
+// }
+
+
+
+//10th question
+
+for(let i =1; i<=10; i++){
+    if(i===2 || i ===8)
+        continue;
+    console.log(i);
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
