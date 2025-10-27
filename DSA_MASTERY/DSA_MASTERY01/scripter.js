2ce410b91c3700593783acd4c31650a245e49811
Akashtheory <akashthawarewhy@gmail.com>
2025-10-27 20:01:11 +0530

    Array based 10 questions

--- DSA_MASTERY/DSA_MASTERY01/scripter.js
+++ DSA_MASTERY/DSA_MASTERY01/scripter.js
@@ -159,12 +159,163 @@
 
 //10th question
 
-for(let i =1; i<=10; i++){
-    if(i===2 || i ===8)
-        continue;
-    console.log(i);
+// for(let i =1; i<=10; i++){
+//     if(i===2 || i ===8)
+//         continue;
+//     console.log(i);
     
-}
+// }
+
+
+
+//.......................................................
+
+// Array 1st Question
+// const numbers = [1,2,3,4,5,6,7,8,9];
+
+// for(let i=0; i<numbers.length; i++){
+//     console.log(numbers[i]);
+// }
+
+
+//2nd Question
+// const arr= [100,200,300,400];
+
+// let sum = 0;
+
+// for(let i =0; i<arr.length; i++){
+//     sum += arr[i]; // add each number to sum
+// }
+
+// console.log('sum:', sum);
+
+// 3rd QUestion
+
+// const fruits = ['Apple', 'ABnana', 'ACheery', 'AWatermalon'];
+
+// for(let i =0; i<fruits.length; i++){
+//     if(fruits[i].startsWith('A')){
+//         console.log(fruits[i]);
+        
+//     }
+// }
+
+
+//4th Question
+// const arr=[1,2,3,4];
+
+// const revarr = [];
+
+// for(let i =arr.length -1; i>=0; i--){
+//     revarr.push(arr[i])
+// }
+// console.log(revarr);
+
+
+//5th Question
+
+// const arr= [5,7,8,9,10,11,12,13,14];
+
+// for (let i=0; i<arr.length; i++){
+//     if(arr[i]>=12){
+//         console.log(arr[i]);
+        
+//     }
+// }
+
+
+//6th question
+
+// const arr=[11,2,3,4,5,6,7,8,9];
+// for(let i=0; i<arr.length; i++){
+//     arr[i] +=7;  //Increase each element by 5
+
+
+// }
+// console.log(arr);
+
+
+//7th question
+
+// const arr=[2,4,239,8,5,16,10];
+
+// let max = arr[0];
+
+// for(let i=1; i<arr.length; i++){
+//     if(arr[i]>max){
+//         max = arr[i]; //Update max of current is greater
+//     }
+// }
+// console.log('Maximum:', max);
+
+
+
+//8th question
+
+// const arr = [2,3,4,5,2,6,6,2,4,2,7, 6, 6];
+
+
+// let count =0;
+
+// for(let i=0; i<arr.length; i++){
+
+//     if(arr[i]===6){
+//         count++;
+//     }
+// }
+
+// console.log(
+//     'number 6 appears:', count, 'times'
+// );
+
+
+
+
+//9th question
+
+// const number =[1,2,3,4,5];
+
+// const doubled =[];
+
+// for(let i=0; i< number.length; i++){
+//     doubled.push(number[i] * 2);
+// }
+
+// console.log(doubled)
+
+
+
+
+//10th question
+
+// const arr= [3,4,77,77,8,5,78, 8989, "Ash", 78];
+
+// let found = false;
+
+// for(let i=0; i<arr.length; i++){
+//     if(arr[i] ===3){
+//         found = true;
+//         break;
+//     }
+// }
+
+// console.log('Contains 7:', found);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
